9cc531172502141dce0abb1fcd1dbf7799191c18
psalmprax <momoholle@compredict.de>
2020-01-14 15:05:07 +0100

    Resolved merge conflict by incorporating both suggestions.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='forecast',
-    version='1..0.0',
+    version='1.0.0',
     packages=['examples', 'compredict', 'compredict.resources'],
     url='',
     license='',
